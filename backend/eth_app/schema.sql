fa744ede51fbe5a88d5fb558627e2aae4a060f3c
thli2739@colorado.edu <thli2739@colorado.edu>
2022-04-16 14:15:39 -0600

    removed previous schema

--- backend/eth_app/schema.sql
+++ backend/eth_app/schema.sql
@@ -1,30 +1,12 @@
-DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS ethentry;
-DROP TABLE IF EXISTS datatype;
 
-
-
-CREATE TABLE datatype (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	title TEXT NOT NULL,
-	units TEXT NOT NULL,
-	description TEXT NOT NULL
-);
 
 CREATE TABLE ethentry (
-	time DATETIME PRIMARY KEY,
-	blocknumber INTEGER NOT NULL,
-	fromaddress VARCHAR(255) NOT NULL,
-	toaddress VARCHAR(255) NOT NULL,
-	ethvalue INTEGER NOT NULL,
-	gas INTEGER NOT NULL,
-	gasUsed INTEGER NOT NULL
+        time INTEGER PRIMARY KEY,
+        blocknumber INTEGER NOT NULL,
+        fromaddress VARCHAR(255) NOT NULL,
+        toaddress VARCHAR(255) NOT NULL,
+        ethvalue INTEGER NOT NULL,
+        gas INTEGER NOT NULL,
+        gasUsed INTEGER NOT NULL
 );
-
-
-
-INSERT INTO datatype (title, units, description)
-	VALUES
-	("Positive Contract Transactions", "Eth", "Total transactions to a contract"),
-	("Negative Contract Transactions", "Eth", "Total transactions from a contract"),
-	("Miner Rewards", "Eth", "Total rewards earned by a miner");
